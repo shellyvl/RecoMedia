3a82c6c71d8ef80430a0662640293fb2798e887c
Shelly-Linda <163829394+shellyvl@users.noreply.github.com>
2026-04-27 16:07:16 +0200

    Delete RecoMedia directory

--- RecoMedia/pages/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // On récupère la session en cours
-session_unset(); // On efface toutes les variables de session (pseudo, etc.)
-session_destroy(); // On détruit techniquement la session sur le serveur
-
-// Redirection vers la page de connexion avec un petit message de confirmation
-header("Location: login.php?deconnecte=1");
-exit();
-?>
